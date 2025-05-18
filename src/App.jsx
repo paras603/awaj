b54250db71a2ad3d6b31416d9a5c377998247bd7
paras603 <parasmanirai1@gmail.com>
2025-05-17 21:46:03 -0400

    sigin in page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { SignIn } from "./components/SignIn"
+
+function App() {
+
+  return (
+    <>
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <SignIn/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
