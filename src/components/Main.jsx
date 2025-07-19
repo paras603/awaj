9427fe5c7f7ecd799ce363f2b9dc3083f9656cd4
paras603 <parasmanirai1@gmail.com>
2025-07-18 23:55:03 -0400

    need to work with image submission

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,6 +14,7 @@ export function Main() {
   function handlePostCreated(newPost) {
     setPosts((prev) => [newPost, ...prev]);
   }
+  
 
   if (loading) return <p className="text-gray-500">Loading posts...</p>;
 
